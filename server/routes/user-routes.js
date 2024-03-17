87beb41d48b5021c010d8cb1945e89ccee89210c
devSeungBin <sb.hong.dev@gmail.com>
2024-03-17 11:55:33 +0900

    test server template engine

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const {  getAllUser , signUp ,logIn } = require("../controller/user-contoller");
+// const { getAllUser , signUp ,logIn } = require("../controller/user-contoller");
 const userRouter = express.Router();
 
+// userRouter.get("/",getAllUser);
+// userRouter.post("/signup", signUp);
+// userRouter.post("/login" , logIn);
 
-userRouter.get("/",getAllUser);
-userRouter.post("/signup", signUp);
-userRouter.post("/login" , logIn);
+userRouter.get('/', (req, res) => {
+    res.render('index')
+})
+
+userRouter.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+userRouter.get('/login', (req, res) => {
+    res.render('login')
+})
 
 module.exports =  userRouter;
