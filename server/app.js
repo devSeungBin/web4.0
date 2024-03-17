ea60c5cb594ccaf443adb1b4954a8b774012e5eb
devSeungBin <sb.hong.dev@gmail.com>
2024-03-18 00:28:41 +0900

    connect db&server (read)

--- server/app.js
+++ server/app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const ejs = require('ejs');
+
 const userRouter = require('./routes/user-routes');
 // const bodyParser = require('body-parser')
-// const mysql = require('mysql2')
-// require('dotenv').config()
 
 const app = express();
 const port = 3000;
 
+// 템플릿 엔진으로 ejs를 사용
 app.set('view engine', 'ejs');
-// view 엔진으로 ejs를 사용
-app.set('views', './views');
+
 // view 파일들은 server/views에 생성
+app.set('views', './views');
 
 app.use('/api', userRouter);
-
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,21 +1,24 @@
 const express = require("express");
-// const { getAllUser , signUp ,logIn } = require("../controller/user-contoller");
 const userRouter = express.Router();
 
-// userRouter.get("/",getAllUser);
-// userRouter.post("/signup", signUp);
-// userRouter.post("/login" , logIn);
+const User = require("../model/User");
+// const { getAllUser, signUp, logIn  } = require('../controller/user-contoller');
 
-userRouter.get('/', (req, res) => {
-    res.render('index')
-})
+userRouter.get('/', async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.render('index', { users });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
 
 userRouter.get('/signup', (req, res) => {
-    res.render('signup')
-})
+    res.render('signup');
+});
 
 userRouter.get('/login', (req, res) => {
-    res.render('login')
-})
+    res.render('login');
+});
 
 module.exports =  userRouter;
